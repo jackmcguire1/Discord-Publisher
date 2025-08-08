cb723a76d7b456fa96ac96c41e3bbd3314c67549
jackmc <jackmcguire35@gmail.com>
2025-08-08 14:29:01 +0100

    fix: fingers crossed

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { resolve } from "path";
 
 export default ({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+  const env = loadEnv(mode, ".", "");
+  const isProd = mode === "production";
+  const repoBase = "/Discord-Publisher/";
 
   return defineConfig({
     plugins: [react()],
-    base: env.VITE_DISCORD_ACTIVITY === "true" ? undefined : "./",
+    base: env.VITE_DISCORD_ACTIVITY === "true" ? undefined : (isProd ? repoBase : "/"),
     server: {
       proxy: {
         "/api": {
@@ -17,15 +18,9 @@ export default ({ mode }) => {
           target: "http://127.0.0.1:8080",
         },
       },
-      base: "./",
     },
     build: {
-      rollupOptions: {
-        input: {
-          main: resolve(__dirname, "index.html"),
-          // nested: resolve(__dirname, "nested/index.html"),
-        },
-      },
+      outDir: "docs",
     },
   });
 };
